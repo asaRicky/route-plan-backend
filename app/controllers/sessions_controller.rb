f57d5ea41fde7b0387d49c6b0a06d05cfe03d10d
nicole <nicolenamayan133@gmail.com>
2023-07-27 09:55:42 +0300

    done with controllers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+    def authenticate
+        user = User.find_by(email: params[:email])
+        if user.nil?
+          render json: { errors: "User not found" }, status: 404
+          return
+        end
+    
+        if user.authenticated?(params[:password])
+          session[:user_id] = user.id
+          render json: { message: "Logged in successfully" }
+        else
+          render json: { errors: "Invalid password" }, status: 401
+        end
+      rescue => e
+        render json: { errors: e.message }, status: :unprocessable_entity
+      end
+    
+      def destroy
+        session[:user_id] = nil
+        render json: { message: "Logged out successfully" }
+      end
+      rescue => e
+        render json: { errors: e.message }, status: :unprocessable_entity
+end
