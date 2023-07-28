49f060ca5519e3df2d7e170fadeffb2706e251a5
nicole <nicolenamayan133@gmail.com>
2023-07-28 11:13:14 +0300

    deleting models

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-    has_many :route_plans
-    has_many :route_visits, through: :route_plans
-end
--- test/controllers/route_visits_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class RouteVisitsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
