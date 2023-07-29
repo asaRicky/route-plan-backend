bf892cc3e2864274573c2579e77451e1a5925add
nicole <nicolenamayan133@gmail.com>
2023-07-29 13:16:18 +0300

    done with merchandisers,salesmanagers,outlets and sessions controllers

--- app/controllers/merchandisers_controller.rb
+++ app/controllers/merchandisers_controller.rb
@@ -1,2 +1,51 @@
 class MerchandisersController < ApplicationController
+    before_action :set_merchandiser, only: [:show, :update, :destroy]
+  
+    # GET /merchandisers
+    def index
+      @merchandisers = Merchandiser.all
+      render json: @merchandisers
+    end
+  
+    # GET /merchandisers/:id
+    def show
+      render json: @merchandiser
+    end
+  
+    # POST /merchandisers
+    def create
+      @merchandiser = Merchandiser.new(merchandiser_params)
+  
+      if @merchandiser.save
+        render json: @merchandiser, status: :created
+      else
+        render json: @merchandiser.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /merchandisers/:id
+    def update
+      if @merchandiser.update(merchandiser_params)
+        render json: @merchandiser
+      else
+        render json: @merchandiser.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /merchandisers/:id
+    def destroy
+      @merchandiser.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def set_merchandiser
+      @merchandiser = Merchandiser.find(params[:id])
+    end
+  
+    def merchandiser_params
+      params.require(:merchandiser).permit(:name, :contact_number)
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   resources :merchandisers
   resources :sales_managers
   resources :outlets
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  post "/register", to: "sessions#authenticate"
+  delete "/logout", to: "sessions#destroy"
+  patch '/reset_password', to: 'users#reset_password'
   
 end
